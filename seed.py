7763111e547bae36479180fa76f88b0c3465864c
jessanettica <jessicalopez@Jessicas-MacBook-Pro-6.local>
2015-09-04 09:50:57 -0700

    user information now available

--- seed.py
+++ seed.py
@@ -8,22 +8,18 @@ from datetime import datetime
 def load_users():
     """Load users from u.user into database."""
 
-    open_user_file = open('seed_data/u.user'):
-
+    open_user_file = open('./seed_data/u.user')
     for row in open_user_file:
         user_info_cell = row.rstrip().split("|")
         new_user_id = user_info_cell[0]
         user_age = user_info_cell[1]
         user_zipcode = user_info_cell[4]
+        email = user_info_cell[5]
+        password = user_info_cell[6]
 
-        new_user = User(user_id=new_user_id, age=user_age, zipcode=user_zipcode)
-    #new_user is the object. new_user is also an instance of the User class. 
-
-#alternative: 
-#row= row.rstrip()
-#user_id, age_gender,occupation, zeipcode = row.split("|")
-#user = User(user_id=user_id, age=age, zipode = zipcode)
-
+        new_user = User(user_id=new_user_id, age=user_age, zipcode=user_zipcode,
+                        email=email, password=password)
+    #new_user is the object. new_user is also an instance of the User class.
 
         db.session.add(new_user)
         #we need to add a session otherwise it will never be stored
@@ -39,19 +35,19 @@ def load_movies():
 # movie_id, title, released_str, junk, imdb_url = row.split("|")[:5]
     for row in open_movie_file:
         new_movie_cell = row.rstrip().split("|")
-        if new_movie_cell[1] =='unknown':
+        if new_movie_cell[1] == 'unknown':
             continue
         else:
             new_movie_id = new_movie_cell[0]
-            new_title= new_movie_cell[1][:-7]
+            new_title = new_movie_cell[1][:-7]
             #Not sure if every title actually has a year, if the title doesn't have a year are we cutting off the last 6 characters of the film's title?
             new_release_date = new_movie_cell[2]
-            d=datetime.strptime(new_release_date, "%d-%b-%Y")
+            d = datetime.strptime(new_release_date, "%d-%b-%Y")
             new_url = new_movie_cell[4]
             new_movie = Movie(movie_id=new_movie_id, title=new_title, released_at=d, imdb_url=new_url)
             db.session.add(new_movie)
-    db.session.commit()  
-    print "Done loading movies! yay!"
+    db.session.commit()
+    print "Done loading movies"
 
 
 def load_ratings():
@@ -64,15 +60,14 @@ def load_ratings():
         new_user_id = movie_info_cell[0]
         new_score = movie_info_cell[2]
 
-        new_rating = Rating(movie_id = new_movie_id, user_id=new_user_id, score=new_score)
-
+        new_rating = Rating(movie_id=new_movie_id, user_id=new_user_id, score=new_score)
 
         db.session.add(new_rating)
     db.session.commit()
     print "Done loading ratings! go go go!"
 
 if __name__ == "__main__":
-    connect_to_db(app) 
+    connect_to_db(app)
 
     load_users()
     load_movies()
